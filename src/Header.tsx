ce78d6786de2525779e74abf34a8f229b8015464
TichonovRoman <romikt@inbox.ru>
2021-12-23 18:14:53 +0300

    9 урок

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (<div>
+            <a href="ere">Home</a>
+            <a href="ere">News Feed</a>
+            <a href="ere">Messages</a>
+        </div>
+    );
+}
+
+export default Header
